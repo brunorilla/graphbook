945dee42670584dd2118151fbded05c78fc8fef1
Bruno Rilla <brunorilla9@gmail.com>
2019-08-03 21:12:54 -0300

    added first production build

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import '../../public/assets/css/styles.css';
+import {Helmet} from "react-helmet";
 
 const posts = [{
         id: 2,
         text: 'Muy buena esta nota',
         user: {
-            avatar: '/uploads/avatar1.png',
+            avatar: 'https://i.pravatar.cc/100',
             username: 'Test User'
         }
     },
@@ -13,7 +14,7 @@ const posts = [{
         id: 1,
         text: "Pésima esta nota",
         user:{
-            avatar: '/uploads/avatar2.png',
+            avatar: 'https://i.pravatar.cc/100',
             username: 'Test User 2'
         }
     }
@@ -36,12 +37,32 @@ export default class App extends Component {
             this.setState({postContent: event.target.value})
         }
 
+        handleSubmit = (event) => {
+            event.preventDefault();
+            const newPost = {
+                id : this.state.posts.length + 1,
+                text : this.state.postContent,
+                user :  {
+                    avatar : 'https://i.pravatar.cc/100',
+                    username : 'Fake User'
+                }
+            };
+            this.setState((prevState) =>({
+                posts : [newPost, ...prevState.posts],
+                postContent: ''
+            }));
+        }
+
         render(){
 
         const {posts, postContent} = this.state;
 
         return(
             <div className="container">
+                <Helmet>
+                    <title>GraphBook - Feed</title>
+                    <meta name="description" content="Newsfeed of all your friends on Graphbook"/>
+                </Helmet>
                 <div className="postForm">
                     <form onSubmit={this.handleSubmit}>
                         <textarea value={postContent} onChange={this.handlePostContentChange} placeholder="Write your custom post!" name="" id="" cols="30" rows="10"></textarea>
