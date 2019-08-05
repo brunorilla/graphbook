3cb361782a5b046c64ae6b2646d8983a4ef96291
Bruno Rilla <brunorilla9@gmail.com>
2019-08-04 21:17:26 -0300

    added helmet, compress and cors packages

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,31 @@
 import express from 'express';
+import path from 'path';
+import helmet from 'helmet';
+import cors from 'cors';
+import compress from 'compression';
 
 const app = express();
 
-app.get('*',(req,res)=> res.send(('Hello World')));
+const root = path.join(__dirname, '../../');
 
+console.log("root: ",root);
+console.log("dirname: ",__dirname);
+
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root,'uploads')));
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(root, '/dist/client/index.html'));
+});
+
+/*
+
+app.get('/', function(req, res, next){
+   console.log('first function');
+    console.log(req);
+   next();
+}, function(req,res){
+    console.log('second function');
+    res.send('Hello world')
+});
+*/
 app.listen(8000, () => console.log('Listening on port 8000!'));
